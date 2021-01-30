005e1ac129fc16c1f1361ad6f7314dcedb1a57b8
Bogumil Bierc <bogumil.bierc@gmail.com>
2021-01-30 12:21:05 +0100

    List all available recipients on modal

--- src/views/scripts/recipients-modal.js
+++ src/views/scripts/recipients-modal.js
@@ -1,4 +1,5 @@
 /*global $*/
+/*global Recipients*/
 
 
 const RecipientsModal = {};
@@ -8,9 +9,31 @@ RecipientsModal.configureFor = (fileName) => {
 }
 
 RecipientsModal.show = () => {
+    RecipientsModal.renderAvailableRecipients();
     $('#add-recipients-modal').modal('show');
 }
 
 RecipientsModal.hide = () => {
     $('#add-recipients-modal').modal('hide');
+}
+
+RecipientsModal.renderAvailableRecipients = () => {
+    $('#available-recipients').empty();
+
+    const recipients = Recipients.getAllRecipients();
+    console.debug('Got recipient for modal');
+    console.debug(recipients);
+
+    recipients.forEach((recipient) => {
+
+        const entryHtml = `<div class="border border-primary border-2 mt-1">
+        <p>${recipient.name}</p>
+        <p>${recipient.address}</p>
+        <button class="btn btn-success">Dodaj</button>
+        </div>
+        `
+        console.debug(`Appending HTML: ${entryHtml}`);
+        $('#available-recipients').append(entryHtml);
+
+    });
 }
